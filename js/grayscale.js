ac68dc78ad9f622e6a04fa3d751bf7daa5332429
Martin Kersner <m.kersner@gmail.com>
2019-11-18 01:19:37 +0900

    update

--- js/grayscale.js
+++ js/grayscale.js
@@ -14,3 +14,19 @@ $('.navbar-collapse ul li a').click(function() {
 $("a").mouseup(function(){
     $(this).blur();
 })
+
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    var about_position = $("#about").offset().top - 20;
+    var projects_position = $("#projects").offset().top;
+
+    if (scroll > about_position && scroll < projects_position) {
+        $(".navbar").addClass("nav-dark");
+        $("#logo").attr("src","img/bisonai-dark.png");
+        $(".navbar-toggle").css({"color": "white"});
+    } else {
+        $(".navbar").removeClass("nav-dark");
+        $("#logo").attr("src","img/bisonai-light.png");
+        $(".navbar-toggle").css({"color": "black"});
+    }
+});
