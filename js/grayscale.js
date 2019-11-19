7afd598aa1815c832f9c3040d7fd2c21731cf187
Martin Kersner <m.kersner@gmail.com>
2019-11-19 20:30:53 +0900

    Transparent logo

--- js/grayscale.js
+++ js/grayscale.js
@@ -22,11 +22,11 @@ $(window).scroll(function() {
 
     if (scroll > about_position && scroll < projects_position) {
         $(".navbar").addClass("nav-dark");
-        $("#logo").attr("src","img/bisonai-dark.png");
+        $("#logo").attr("src","img/bisonai-transparent-dark.png");
         $(".navbar-toggle").css({"color": "white"});
     } else {
         $(".navbar").removeClass("nav-dark");
-        $("#logo").attr("src","img/bisonai-light.png");
+        $("#logo").attr("src","img/bisonai-transparent-light.png");
         $(".navbar-toggle").css({"color": "black"});
     }
 });
